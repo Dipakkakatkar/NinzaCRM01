1f7b477fb67a4af74342ae9b780e626afa5b36f2
Dipak <kakatkardipak@gmail.com>
2026-04-25 17:54:47 +0530

    First Commit

---
+++ src/test/java/Demo/PracTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class PracTest {
+	
+	
+	@Test(groups="regression")
+	public void appleTest() {
+		System.out.println("apple");
+	}
+
+}
